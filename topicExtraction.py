d29e7ad6e3246d7770b9b6e87f1539670868af1f
mtsuboor <thetalhasuboor@gmail.com>
2021-09-19 00:07:51 -0500

    finished entity parsing

--- topicExtraction.py
+++ topicExtraction.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 from google.cloud import language_v1
 
-data_set = []  
+data_set = [] 
 cleaned_set = set()
 name_set = set()
 
@@ -27,10 +27,43 @@ def sample_analyze_entities(text_content):
 
     # Loop through entitites returned from the API
     # print(response.entities)
+
     for entity in response.entities:
-        data_set.append((u"{}".format(entity.name), u"{}".format(entity.salience)))
-        cleaned_set.add((u"{}".format(entity.name), 0))
-        name_set.add(u"{}".format(entity.name))
+      #issue: this condition is always true: but why?
+      # if u"{}".format(entity.name) not in data_set:
+      if float(entity.salience) >= 0.01:
+        data_set.append([u"{}".format(entity.name), u"{}".format(entity.salience)])
+        # cleaned_set.add((u"{}".format(entity.name),  u"{}".format(entity.salience)))
+      #   name_set.add(u"{}".format(entity.name))
+      # else:
+      #   tempSalience = u"{}".format(entity.salience)
+      #   data_set[u"{}".format(entity.name)][2] = data_set[u"{}".format(entity.name)][2] + 1
+      #   data_set[u"{}".format(entity.name)][1] = data_set[u"{}".format(entity.name)][1] + tempSalience
+    print(data_set,"\n")
+    print("dataset Length is: ", len(data_set))
+    i = 0      
+    while 1:
+      x = i + 1
+      if i == len(data_set):
+        break
+      print(i)
+      print(len(data_set))
+      uniqEnt = data_set[i][0]
+      uniSal = float(data_set[i][1])
+      # print("starting hunt for clones at index: ", x)
+      while 1:
+        if x == len(data_set):
+          break
+        if data_set[x][0] == uniqEnt :
+          data_set.remove(data_set[x])
+        x = x + 1
+      i = i + 1
+
+
+    # for uniques in data_set:
+    #   print(uniques[0])
+      # data_set[u"{}".format(uniques[0])][1] = (data_set[u"{}".format(uniques[0])][1])/ data_set[u"{}".format(uniques[0])][2]
+
         # Get the salience score associated with the entity in the [0, 1.0] range
 
         # print(u"{}".format(entity.name), u"{}".format(entity.salience))
@@ -61,8 +94,8 @@ def sample_analyze_entities(text_content):
 
 message = 'On Aug. 15, Prime Minister Justin Trudeau visited newly minted Governor General Mary Simon in order to dissolve Parliament and call a snap election. He argued that this election would allow Canadians to have a say in who should guide Canadians out of the pandemic.Of course, the prime minister’s greater goal was to seize the (perceived) opportunity to win a majority government. Neither the arrival of the election, nor the prime minister’s motives, were a surprise to Canadians. But Trudeau himself appears surprised to learn that Canadians did not want an election, which is clearly having a detrimental impact on Liberal fortunes in the polls.Historically, Conservative leaders typically own the unenviable label of being the leader that Canadians believe has a hidden agenda. However, Canadians say it is the Liberal leader who has the ulterior motive this time around.Read more: Pandemic elections — How have other countries voted during COVID-19? The Liberals were in a strong position heading into the election. Approval ratings of Trudeau’s performance have been strong, on the heels of a successful rollout of the COVID-19 vaccine in Canada. Moreover, Ipsos polling throughout 2021 has placed the Liberals in the driver’s seat, at one point enjoying a double-digit lead over the second-place Conservatives in the national popular vote. Earlier in summer, the gap between the leading parties shrunk to six points. If the Liberals could win a plurality of seats and form a minority government despite having lost the popular vote to the Tories in 2019, then even a six-point lead over the Conservatives should give them a good shot at forming a majority government, right? Wrong. The miscalculation of the Liberal Party in believing that Canadians would be okay with an election campaign amidst the fourth wave of the pandemic might be their undoing, or at the very least, prevent them from winning a majority mandate. At the start of the campaign, 56 per cent of Canadians said that we should not be having an election during a pandemic. In the absence of a compelling argument from Trudeau articulating why an election is necessary, the proportion of Canadians who feel we shouldn’t be in a campaign right now has grown to 69 per cent. Moreover, one in four Canadians say they do not feel safe voting in person. Exacerbating the challenges of the Liberal government is the absence of a dominant wedge issue. Canadians say that the pandemic, affordability, the economy, health care, housing and climate change are among the most important issues to them. Liberal Leader Trudeau has tried to create a wedge issue out of a number of these, including health care, climate change, gun control and abortion rights, but Conservative Party Leader Erin O’Toole has largely avoided the wedge by saying that he supports the health-care system Canada currently has, that he believes in climate change, and that he is pro-choice. The gun control wedge is working somewhat within Quebec, preventing the Conservatives from gaining too much traction in la belle province. Therefore, we’re left with an election campaign with no clear top issue to drive the narrative. Without a key wedge between himself and O’Toole, Trudeau is once again answering what has become a familiar question in this campaign: why are we having an election? Through their shifting vote intentions compared to the start of the campaign, Canadians are indicating that a compelling answer remains elusive. Read more: Here’s what’s different about this year’s federal election in Canada. The election has given Canadians an opportunity to get a closer look at the other party leaders. Over the last 18 months, the prime minister has dominated the news — and rightly so — as Canadians have rallied around their leaders during these challenging circumstances. This left O’Toole, NDP Leader Jagmeet Singh and the other leaders struggling for airtime. The campaign has put a spotlight on the opposition leaders, and Canadians like what they’re seeing. Singh is seen as the most likeable of all the party leaders, while perceptions of O’Toole are also improving, as many Canadians believe that he isn’t the boogeyman that Trudeau is trying to portray him as. The solid lead the Liberals once enjoyed has evaporated, and we have a horserace on our hands. There is a good chance that we will end up back where we started, with a Liberal minority government. This is likely the best-case scenario for the Liberals. And if they do win with a Parliament that looks a lot like the previous one, Trudeau may once again need to explain to Canadians why we had an election. Not an enviable position for the start of one’s next mandate.'
 sample_analyze_entities(message)
-# print(data_set)
-# print(name_set)
+print(data_set)
+# print(cleaned_set)
 
 
 # def average_salience(names):
