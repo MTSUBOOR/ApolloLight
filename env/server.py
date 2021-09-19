0089e18b3e4b571018b3785d55944e594e2bb8df
mtsuboor <thetalhasuboor@gmail.com>
2021-09-18 22:07:23 -0500

    sent analy boilerplate

--- env/server.py
+++ env/server.py
@@ -4,7 +4,7 @@ from google.cloud import language_v1
 client = language_v1.LanguageServiceClient()
 
 # The text to analyze
-text = u"Hello, world!"
+text = "Hello, world!"
 document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
 
 # Detects the sentiment of the text
